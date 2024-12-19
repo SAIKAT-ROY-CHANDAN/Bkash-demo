f75447c7749168a6237f7a1698d4f06184cb89a1
SAIKOT-ROY <saikotroydev@gmail.com>
2024-12-19 20:39:17 +0600

    some line commented to checkout

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,24 +1,24 @@
-import axios from "axios";
+// import axios from "axios";
 
 const Checkout = () => {
-  const pay = async () => {
-    try {
-      const { data } = await axios.post(
-        "http://localhost:5000/api/bkash/payment/create",
-        { amount: 50, orderId: 1 },
-        { withCredentials: true }
-      );
+  // const pay = async () => {
+  //   try {
+  //     const { data } = await axios.post(
+  //       "http://localhost:5000/api/bkash/payment/create",
+  //       { amount: 50, orderId: 1 },
+  //       { withCredentials: true }
+  //     );
 
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   return (
     <div>
       <button 
-      onClick={pay}
+      // onClick={pay}
       className="bg-black/80 text-white px-6 py-3 rounded-md hover:bg-black/90">
         Pay with Bkash
       </button>
